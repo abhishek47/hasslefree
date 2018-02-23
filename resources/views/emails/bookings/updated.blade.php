491851f6ef0de67652810af83a5f2e33a3ab7302
abhishek47 <abhishekwani@hotmail.com>
2018-02-23 13:24:57 +0530

    disclaimer

--- resources/views/emails/bookings/updated.blade.php
+++ resources/views/emails/bookings/updated.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Booking {{$booking->id}} : {{ getStatusString($booking->status) }}
 
-Your booking status with ID {{$booking->id}} has been updated to : {{ getStatus($booking->status) }}
+Your booking status with ID {{$booking->id}} has been updated to : {!! getStatus($booking->status) !!}
 
 @component('mail::button', ['url' => '/bookings/' . $booking->id])
 View Details
