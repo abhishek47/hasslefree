a0722f76a52c67e07ddf91da3a3321842c51da68
abhishek47 <abhishekwani@hotmail.com>
2018-03-18 21:11:01 +0530

    invest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -261,17 +261,29 @@
                                     <label>Drop time</label>
                                     <select class="form-control" name="drop_time" id="drop_time" onchange="checkTimeDifference();">
                                         <option selected disabled>--- Choose Time ---</option>
-                                        <option value="1">09:00 AM - 10:00 AM</option>
-                                        <option value="2">10:00 AM - 11:00 AM</option>
-                                        <option value="3">11:00 AM - 12:00 PM</option>
-                                        <option value="4">12:00 PM - 01:00 PM</option>
-                                        <option value="5">01:00 PM - 02:00 PM</option>
-                                        <option value="6">02:00 PM - 03:00 PM</option>
-                                        <option value="7">03:00 PM - 04:00 PM</option>
-                                        <option value="8">04:00 PM - 05:00 PM</option>
-                                        <option value="9">05:00 PM - 06:00 PM</option>
-                                        <option value="10">06:00 PM - 07:00 PM</option>
-                                        <option value="11">07:00 PM - 08:00 PM</option>
+                                        <option value="1">12:00 AM - 01:00 AM</option>
+                                        <option value="2">01:00 AM - 02:00 AM</option>
+                                        <option value="3">02:00 AM - 03:00 AM</option>
+                                        <option value="4">03:00 AM - 04:00 AM</option>
+                                        <option value="5">04:00 AM - 05:00 AM</option>
+                                        <option value="6">05:00 AM - 06:00 AM</option>
+                                        <option value="7">07:00 AM - 08:00 AM</option>
+                                        <option value="8">08:00 AM - 09:00 AM</option>
+                                        <option value="9">09:00 AM - 10:00 AM</option>
+                                        <option value="10">10:00 AM - 11:00 AM</option>
+                                        <option value="11">11:00 AM - 12:00 PM</option>
+                                        <option value="12">12:00 PM - 01:00 PM</option>
+                                        <option value="13">01:00 PM - 02:00 PM</option>
+                                        <option value="14">02:00 PM - 03:00 PM</option>
+                                        <option value="15">03:00 PM - 04:00 PM</option>
+                                        <option value="16">04:00 PM - 05:00 PM</option>
+                                        <option value="17">05:00 PM - 06:00 PM</option>
+                                        <option value="18">06:00 PM - 07:00 PM</option>
+                                        <option value="19">07:00 PM - 08:00 PM</option>
+                                        <option value="20">08:00 PM - 09:00 PM</option>
+                                        <option value="21">09:00 PM - 10:00 PM</option>
+                                        <option value="22">10:00 PM -  11:00 PM</option>
+                                        <option value="23">11:00 PM - 12:00 AM</option>
                                     </select>
                                 </div>
                             </div>
