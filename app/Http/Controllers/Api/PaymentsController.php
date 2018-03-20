936d3a677134ce04e39d3be1e92f08c5889805ba
abhishek47 <abhishekwani@hotmail.com>
2018-03-20 09:20:20 +0530

    reset

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -6,6 +6,7 @@ use App\Models\Booking;
 use Illuminate\Http\Request;
 use App\Mail\NewBookingCreated;
 use Softon\Indipay\Facades\Indipay;  
+use App\Http\Controllers\Controller;
 
 class PaymentsController extends Controller
 {
