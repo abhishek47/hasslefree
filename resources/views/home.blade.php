6e7ddc24b644411e2d3aec4c30a1ed3dcc0681e8
abhishek47 <abhishekwani@hotmail.com>
2018-03-22 17:31:26 +0530

    for-schools

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="bg-light mini-spacer" style="min-height: 1000px;">
+<div class="bg-light mini-spacer" style="min-height: 1000px;padding-top: 90px;">
     <div class="container">
 
         <div class="row hidden-md-down">
