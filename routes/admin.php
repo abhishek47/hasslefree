8251ea19329776c43abc0ff0433237cacbbe2486
abhishek47 <operations@trumpetstechnologies.com>
2018-05-20 16:40:33 +0530

    defects2

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 CRUD::resource('airports', 'AirportCrudController');
 CRUD::resource('bookings', 'BookingCrudController');
+CRUD::resource('customers', 'CustomerCrudController');
 CRUD::resource('refunds', 'RefundCrudController');
 CRUD::resource('bus-stations', 'BusstationCrudController');
 CRUD::resource('train-stations', 'TrainStationCrudController');
